7de1260dd0194fff4ac1e7993f4a263ac3ed9d1e
Muhammad Nur Ichsan <mnichsaan@gmail.com>
2020-11-08 21:31:15 +0700

    update test.vue
    create simple hero

--- pages/test.vue
+++ pages/test.vue
@@ -2,8 +2,23 @@
 	<div>
 		<div class="container mt-10">
 			<div class="grid grid-cols-12 ml-10 mr-10">
-				<div class="col-span-6 flex justify-center items-center">
-					<p class="text-justify">Get Healthy Everyday With Us</p>
+				<div class="col-span-6 row-span-6">
+					<p
+						class="text-justify md:text-3xl sm:text-sm italic md:mt-24 ml-32 sm:mt-0"
+					>
+						Get Healthy Everyday With Us,
+						<br />
+						<strong>Clinix</strong>
+					</p>
+					<p class="text-justify ml-32 mt-3">
+						we provide an easier way to manage your healthy lifestyle as easy
+						just one click
+					</p>
+					<button
+						class="bg-gray-700 rounded ml-32 mt-2 py-2 px-3 hover:bg-gray-500"
+					>
+						Get Started
+					</button>
 				</div>
 				<div class="col-span-6 flex justify-center">
 					<img src="img/logo.png" alt="" class="w-2/4" />

30a31f539baca1ee0226bc3e94afe6b0d678a034
Muhammad Nur Ichsan <mnichsaan@gmail.com>
2020-11-08 20:27:00 +0700

    update component navbar and create test page
    add container for navbar,test page for testing tailwind and make some cool user interface

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,34 +1,42 @@
 <template>
-  <div class="w-100 h-25 p-2 pt-3 flex items-center justify-between">
-    <div class="flex items-center">
-      <img src="img/logo.png" alt=""
-           class="w-10 sm:w-12 md:w-20"
-           :style="$colorMode.value === 'dark' ? 'filter: invert(1)' : ''">
-      <p class="text-2xl sm:text-3xl ml-3">Clinix</p>
-    </div>
-    <button
-      class="bg-gray-700 hover:bg-gray-600 dark:bg-gray-400 text-white font-bold py-2 px-4 rounded-full transition-all duration-300"
-      @click="toggleDarkMode()">
-      <span v-if="$colorMode.value === 'light'">
-        <fa :icon="['far', 'moon']" class="pr-1" /> Dark Mode
-      </span>
-      <span v-else>
-        <fa :icon="['far', 'sun']" class="pr-1" /> Light Mode
-      </span>
-    </button>
-  </div>
+	<div class="container">
+		<div
+			class="w-100 h-25 p-2 pt-3 flex items-center justify-between ml-10 mr-10"
+		>
+			<div class="flex items-center">
+				<img
+					src="img/logo.png"
+					alt=""
+					class="w-10 sm:w-12 md:w-20"
+					:style="$colorMode.value === 'dark' ? 'filter: invert(1)' : ''"
+				/>
+				<p class="text-2xl sm:text-3xl ml-3">Clinix</p>
+			</div>
+			<button
+				class="bg-gray-700 hover:bg-gray-600 dark:bg-gray-400 text-white font-bold py-2 px-4 rounded-full transition-all duration-300"
+				@click="toggleDarkMode()"
+			>
+				<span v-if="$colorMode.value === 'light'">
+					<fa :icon="['far', 'moon']" class="pr-1" /> Dark Mode
+				</span>
+				<span v-else>
+					<fa :icon="['far', 'sun']" class="pr-1" /> Light Mode
+				</span>
+			</button>
+		</div>
+	</div>
 </template>
 Œ
 <script>
-export default {
-  methods: {
-    toggleDarkMode() {
-      if (this.$colorMode.value !== 'dark') {
-        this.$colorMode.value = 'dark'
-      } else {
-        this.$colorMode.value = 'light'
-      }
-    }
-  }
-}
+	export default {
+		methods: {
+			toggleDarkMode() {
+				if (this.$colorMode.value !== "dark") {
+					this.$colorMode.value = "dark";
+				} else {
+					this.$colorMode.value = "light";
+				}
+			},
+		},
+	};
 </script>
---
+++ pages/test.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div class="container mt-10">
+			<div class="grid grid-cols-12 ml-10 mr-10">
+				<div class="col-span-6 flex justify-center items-center">
+					<p class="text-justify">Get Healthy Everyday With Us</p>
+				</div>
+				<div class="col-span-6 flex justify-center">
+					<img src="img/logo.png" alt="" class="w-2/4" />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style>
+</style>

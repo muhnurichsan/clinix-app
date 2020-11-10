e3e40152aca2f88d166e7d68c90007861953b97e
Muhammad Nur Ichsan <mnichsaan@gmail.com>
2020-11-10 19:35:41 +0700

    [FIX #3] UI - Create calorie calculator page
    create calories page and fix typo in hydrated page

--- pages/calories-calculator.vue
+++ pages/calories-calculator.vue
@@ -105,6 +105,21 @@
 						<div class="w-full md:w-1/2 px-3 mb-6 md:mb-6">
 							<label
 								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+								for="grid-age"
+							>
+								Age
+							</label>
+							<input
+								class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+								id="grid-age"
+								type="number"
+								placeholder="your age"
+								name="ageWoman"
+							/>
+						</div>
+						<div class="w-full md:w-1/2 px-3 mb-6 md:mb-6">
+							<label
+								class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
 								for="grid-height"
 							>
 								Height
